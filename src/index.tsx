54f83ce47ddb4639451ae4ff022f1040125b6e8e
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-04-27 19:01:27 +0300

    fix: case

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from './App';
+import App from './a_pp';
 import reportWebVitals from './reportWebVitals';
 
 import './index.css';
