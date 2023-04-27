581f1542235293519a5cb83a340d583c3acb58f6
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-04-27 19:02:03 +0300

    fix: kebab-case

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from './a_pp';
+import App from './app';
 import reportWebVitals from './reportWebVitals';
 
 import './index.css';
