0f8e5ff973903b4d73fb358b3e6618e2dbcb1f46
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-05-03 22:19:42 +0300

    fix: drafts for pages

--- src/layers/main/main.jsx
+++ src/layers/main/main.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+import { useSelector, useDispatch } from 'react-redux';
 
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
@@ -10,26 +11,37 @@ import BurgerConstructor from '../../components/burger-constructor';
 import Preloader from '../../components/preloader';
 
 import useQuery from '../../hooks/use-query';
+import { selectIngredient, setIngredient } from '../../store/slices/ingredient-slice';
 import useWindowDimensions, { getVisualProps } from '../../hooks/use-window-dimensions';
 import { cardPropTypes } from '../../utils/types';
 
 import style from './main.module.css';
 
 export default function Main() {
+  const dispatch = useDispatch();
+
   const [items, setItems] = useState([]);
   const [currentBun, setCurrentBun] = useState(null);
   const [sum, setSum] = useState(0);
   const { blocks } = getVisualProps(useWindowDimensions());
   const isMobile = blocks === 1;
   const { data: cards, hasError, isLoading } = useQuery({ url: 'ingredients' });
+  
 
-  useEffect(() => console.log(cards, hasError, isLoading), [isLoading]);
+  useEffect(() => {
+    dispatch(setIngredient(cards));
+    console.log(cards, hasError, isLoading);
+  }, [isLoading]);
+
   useEffect(() => {
     const summ = items.reduce((sumPrice, x) => (sumPrice + x.price), 0);
     const result = summ + (currentBun?.price ? currentBun.price : 0);
     setSum(result);
     console.log(result);
   }, [items.length, currentBun]);
+
+  const data = useSelector(selectIngredient);
+  console.log('ingredient', data);
 
   // preloader
   return (isLoading
