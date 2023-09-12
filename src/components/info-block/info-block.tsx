f4432f346c46d0c1f2c5bfa26a68128b7d84dc4c
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-09-12 08:03:35 +0300

    fix: info-block

--- src/components/info-block/info-block.tsx
+++ src/components/info-block/info-block.tsx
@@ -3,14 +3,12 @@ import React from 'react';
 import List from '../list';
 import InfoBlockTotal from '../info-block-total';
 
-import { useAppSelector } from '../../hooks';
-import { selectOrders } from '../../store/slices';
+import { useOrders } from '../../hooks';
 
 import style from './info-block.module.css';
 
 export default function InfoBlock() {
-  const ordersData = useAppSelector(selectOrders);
-  const { orders = [], total = 0, totalToday = 0 } = ordersData!;
+  const { orders = [], total = 0, totalToday = 0 } = useOrders();
   const done = orders.filter((x) => x.status === 'done').map((x) => (x.number.toString()));
   const backlog = orders.filter((x) => x.status !== 'done').map((x) => (x.number.toString()));
 
