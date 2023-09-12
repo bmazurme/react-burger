c8c1fb05c88e80fe765f3b7fbf871c9553fe77fe
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-09-12 08:01:25 +0300

    fix: hooks

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,7 +1,19 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+
 import type { RootState, AppDispatch } from '../store';
+import useBurger from './use-burger';
+import useUser from './use-user';
+import useOrders from './use-orders';
+import useIngredient from './use-ingredient';
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
 type DispatchFunc = () => AppDispatch;
 export const useAppDispatch: DispatchFunc = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+export {
+  useBurger,
+  useUser,
+  useOrders,
+  useIngredient,
+};
